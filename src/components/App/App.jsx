d5a66f61eda79fef1ee46cce37e8bc9c40f89bb9
Bennett Smrdel <smrdelb@gmail.com>
2021-12-02 16:32:31 -0600

    done with branch

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -15,7 +15,7 @@ function App() {
   
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
-      <p> Joe is also great. </p>
+      <p> Joe is also great. </p> 
       <PizzaSelect />
     </div>
   );
