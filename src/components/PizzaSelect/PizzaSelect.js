55e7c467d0879eedb3b67c967672035158635a52
Bennett Smrdel <smrdelb@gmail.com>
2021-12-02 14:10:17 -0600

    created 3 unique pizza cards to show the different types of pizza

--- src/components/PizzaSelect/PizzaSelect.js
+++ src/components/PizzaSelect/PizzaSelect.js
@@ -8,13 +8,64 @@ function PizzaSelect() {
     return (
         <>
             <h3>Step 1: Select Your Pizza!</h3>
+            {/* Pepperoni pizza */}
             <Card sx={{ maxWidth: 345 }}>
             <CardActionArea>
                 <CardMedia
                 component="img"
                 height="140"
-                image="/static/images/cards/contemplative-reptile.jpg"
-                alt="green iguana"
+                image="images/pizza_01.jpeg"
+                alt="pizza_01"
+                />
+                <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                Share
+                </Button>
+            </CardActions>
+            </Card>
+            {/* Sausage pizza */}
+            <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+                <CardMedia
+                component="img"
+                height="140"
+                image="images/pizza_02.jpeg"
+                alt="pizza_02"
+                />
+                <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                Share
+                </Button>
+            </CardActions>
+            </Card>
+            {/* Pizza pineapple */}
+            <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+                <CardMedia
+                component="img"
+                height="140"
+                image="images/pizza_03.jpeg"
+                alt="pizza_03"
                 />
                 <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
